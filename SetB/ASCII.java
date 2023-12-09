3e97f903f7c4f32703b23ef4b873fc7476f32c4b
Sushant Kumbhar <142664990+Sushantk7798@users.noreply.github.com>
2023-12-09 18:42:44 +0530

    Add files via upload

---
+++ SetB/ASCII.java
@@ -0,0 +1,13 @@
+package SetB;
+
+public class ASCII {
+
+	public static void main(String[] args) {
+
+		char ch = '}';
+
+		int ascii = ch;
+
+		System.out.println("The ASCII value of " + ch + " is: " + ascii);
+	}
+}
