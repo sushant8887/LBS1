ae2eae8c6f88c9b59f8109bbafe27f97e9f64fa4
Sushant Kumbhar <142664990+Sushantk7798@users.noreply.github.com>
2023-12-10 05:52:28 +0530

    Update DeciToOct.java

--- SetB/DeciToOct.java
+++ SetB/DeciToOct.java
@@ -1,28 +1,19 @@
 package SetB;
-
 import java.io.*;
-
 class DeciToOct {
-
 	static void decToOctal(int n) {
-
 		int[] octalNum = new int[100];
-
 		int i = 0;
 		while (n != 0) {
-
 			octalNum[i] = n % 8;
 			n = n / 8;
 			i++;
 		}
-
 		for (int j = i - 1; j >= 0; j--)
 			System.out.print(octalNum[j]);
 	}
-
 	public static void main(String[] args) {
 		int n = 33;
 
 		decToOctal(n);
-	}
-}
+	}}
