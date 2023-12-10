de5d44558aff027a7e92f6c5119d56216724eadf
Sushant Kumbhar <142664990+sushant8887@users.noreply.github.com>
2023-12-10 06:34:34 +0530

    Update Leapyear.java

--- SetB/Leapyear.java
+++ SetB/Leapyear.java
@@ -1,15 +1,14 @@
 import java.util.Scanner;
-public class Leapyear {
-public static void main(String[] args) {
-	int y;
-	Scanner sc = new Scanner(System.in);
-	System.out.println("Enter the valid year");
-	y = sc.nextInt();
-	if(y%4 == 0) {
-		System.out.println("Leap year");
-	}
-	else {
-		System.out.println("not a leap year");
-	}
-}
+public class LeapYear {
+   public static void main(String[] args){
+      int year;
+      System.out.println("Enter an Year :: ");
+      Scanner sc = new Scanner(System.in);
+      year = sc.nextInt();
+
+      if (((year % 4 == 0) && (year % 100!= 0)) || (year%400 == 0))
+         System.out.println("Specified year is a leap year");
+      else
+         System.out.println("Specified year is not a leap year");
+   }
 }
